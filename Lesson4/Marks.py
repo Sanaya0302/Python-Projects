7dc91339b3666246d5a54a71d8bcf62b4601f369
Sanaya0302 <sanaya03feb@gmail.com>
2025-08-07 21:45:45 +0530

    Count money

---
+++ Lesson4/Marks.py
@@ -0,0 +1,7 @@
+math=int(input("Enter marks "))
+english=int(input("Enter marks "))
+kannada=int(input("Enter marks "))
+sum=math+english+kannada
+avg=sum/3
+per=(sum/100)*100
+print("average marks is ",avg,"and your % is",per)
