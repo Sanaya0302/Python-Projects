6aa7b2ca0e5bd75a830c29106b18e7890f5d20b9
Sanaya0302 <sanaya03feb@gmail.com>
2025-08-14 21:32:12 +0530

    Lesson 6

---
+++ Lesson 5/HW LESSON 5/Lesson 6/Activity 2.py
@@ -0,0 +1,12 @@
+a=-10
+b=12
+c=0
+if a and b and c:
+    print("All the numbers are true")
+else:
+    print("Atleast one number is false")
+if a>0 or b>0:
+    print("Ether one of the values a greater then 0")
+else:
+    print("Both of the values are less then 0")
+print(a !=b)
