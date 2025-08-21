2e20e15767165b8154cd302f4bcd46f4d895018c
Sanaya0302 <sanaya03feb@gmail.com>
2025-08-21 21:41:22 +0530

    Lesson 7/8

---
+++ HW Checking Alphabets/Lesson 8/Activity 1.py
@@ -0,0 +1,8 @@
+tot_num=40
+mean1=38
+wrong_num=36
+correct_num=56
+sum=tot_num*mean1
+sum=sum+correct_num-wrong_num
+mean2=sum/40
+print("Correct mean =",mean2)
--- Lesson 7/Activity 2.py
+++ Lesson 7/Activity 2.py
@@ -1,7 +1,10 @@
 x=10
 y=16
-print(x is y)
-print(x is not y)
-z=[2,4,6,8,10]
-print(x in z)
-print(x not in z)
+# print(x is y)
+# print(x is not y)
+# z=[2,4,6,8,10]
+# print(x in z)
+# print(x not in z)
+
+print(x << 1)
+print(x >> 1)
