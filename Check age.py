f7ac1d37a37604c8ffee0b1a22ce056187c34910
Sanaya0302 <sanaya03feb@gmail.com>
2025-09-09 21:30:31 +0530

    activity

--- Check age.py
+++ Check age.py
@@ -2,6 +2,7 @@ age=int(input("Write your age: "))
 if age<10:
     print("You are not allowed")
 if age>10:
-    print("You are allowed")
-if age>20:
-    print("You are not allowed")
+    if age<20:
+        print("You are allowed")
+    else:
+        print("You are not allowed")
