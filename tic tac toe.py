ec3d24ec5b2b918fe03d1f7c5e1c2f497f0e02be
Sanaya0302 <sanaya03feb@gmail.com>
2025-11-18 21:38:29 +0530

    tic

--- tic tac toe.py
+++ tic tac toe.py
@@ -18,6 +18,64 @@ def game():
         move=input()
         if the_board[move]==" ":
             the_board[move]=turn
+            count=count+1
         else:
             print("put it somewhere else!!!!!!!!")
             continue
+        if count>=5:
+            if the_board['1']==the_board['2']==the_board['3']!=' ':
+                print_board(the_board)
+                print("game over")
+                print('****'+turn+'won****')
+                break
+            elif the_board['4']==the_board['5']==the_board['6']!=' ':
+                print_board(the_board)
+                print("game over")
+                print('****'+turn+'won****')
+                break
+            elif the_board['7']==the_board['8']==the_board['9']!=' ':
+                print_board(the_board)
+                print("game over")
+                print('****'+turn+'won****')
+                break
+            elif the_board['1']==the_board['4']==the_board['7']!=' ':
+                print_board(the_board)
+                print("game over")
+                print('****'+turn+'won****')
+                break
+            elif the_board['2']==the_board['5']==the_board['8']!=' ':
+                print_board(the_board)
+                print("game over")
+                print('****'+turn+'won****')
+                break
+            elif the_board['3']==the_board['6']==the_board['9']!=' ':
+                print_board(the_board)
+                print("game over")
+                print('****'+turn+'won****')
+                break
+            elif the_board['1']==the_board['5']==the_board['9']!=' ':
+                print_board(the_board)
+                print("game over")
+                print('****'+turn+'won****')
+                break
+            elif the_board['3']==the_board['5']==the_board['7']!=' ':
+                print_board(the_board)
+                print("game over")
+                print('****'+turn+'won****')
+                break
+        if count==9:
+            print("Game over!!!!!!!!!!!!!!")
+            print("Its a tie!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        if turn=="X":
+            turn="O"
+        else:
+            turn="X"
+        playagain=input("do you want to play again?(y/n)")
+        if playagain=="y" or playagain=="Y":
+            for key in board_keys:
+                the_board[key]=' '
+        else:
+            print("bye bye!!!!!!!!!!!!!!!!!!!")
+game()
+if __name__=="__main__":
+    game()
