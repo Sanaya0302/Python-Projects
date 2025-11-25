642a585fe2d72483144fc22fd894c9fad681310a
Sanaya0302 <sanaya03feb@gmail.com>
2025-11-25 21:40:53 +0530

    pair

---
+++ pairs.py
@@ -0,0 +1,9 @@
+class pairs:
+    def twosum(self,nums,target):
+        lookout={}
+        for i,num in enumerate(nums):
+            if target-num in lookout:
+                return lookout[target-num],i
+            lookout[target-num]=i
+target=int(input("Enter a num between 10 and 130: "))
+print("index1=%d index2=%d" % pairs().twosum((10,20,30,40,50,60,70),target))
