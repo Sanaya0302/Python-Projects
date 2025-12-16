216517305da941b9a7facbdf1c91485c12b16641
Sanaya0302 <sanaya03feb@gmail.com>
2025-12-16 21:41:00 +0530

    quizz

---
+++ Lesson 5/HW LESSON 5/Lesson 6/quiz.py
@@ -0,0 +1,18 @@
+import random
+class quiz():
+    def __init__(self):
+        self.fruits={"apple":"red","strawberry":"red","watermelon":"green","lemon":"yellow","pineapple":"yellow","orange":"orange","banana":"yellow"}
+    def quizzz(self):
+        while (True):
+            fruit,colour=random.choice(list(self.fruits.items()))
+            print("What is the colour of ",fruit)
+            ans=input()
+            if(ans.lower()==colour):
+                print("corect answer!!!!!")
+            else:
+                print("wrong answer!!!!!!")
+            option=int(input("Enter 1 if you want to play again else type 0: "))
+            if 0:
+                break
+q=quiz
+q.quizzz()
