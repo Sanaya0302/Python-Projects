f02c6f38e4252e044d140127fcce111056615d1c
Sanaya0302 <sanaya03feb@gmail.com>
2025-12-18 18:05:08 +0530

    agshdfagh

---
+++ HW Lesson 4/cars.py
@@ -0,0 +1,16 @@
+class BMW:
+    def max_speed(self):
+        print("BMW max speed is 250 km/h")
+    def fuel_type(self):
+        print("BMW uses petrol")
+class Ferrari:
+    def max_speed(self):
+        print("ferrari max speed is 340 km/h")
+    def fuel_type(self):
+        print("ferrari uses diesel")
+bmw=BMW()
+ferrari=Ferrari()
+bmw.fuel_type()
+bmw.max_speed()
+ferrari.fuel_type()
+ferrari.max_speed()
