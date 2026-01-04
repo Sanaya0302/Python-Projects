1479918325c465767a62406815473c6199d67d1b
Sanaya0302 <sanaya03feb@gmail.com>
2026-01-04 12:38:23 +0530

    whb

---
+++ shapes.py
@@ -0,0 +1,13 @@
+import pygame
+def main():
+    pygame.init()
+    width,height=500,500
+    screen=pygame.display.set_mode((width,height))
+    pygame.display.set_caption("My first game screen!")
+    color={
+        'yellow':pygame.Color('yellow')}
+    current=color['yellow']
+    screen.fill((0,0,0))
+    rect=250,250
+    pygame.draw.rect(screen,current(rect))
+main()
